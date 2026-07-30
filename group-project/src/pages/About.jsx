25b9b7077899b2c4c19fb91ac8a671e34151bfb4
Epicmasonic <epicmasonic@gmail.com>
2026-07-30 12:22:35 -0400

    Technically add an about page

---
+++ group-project/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import data from '../data/db.json'
+import './About.css'
+
+const About = () => {
+  return (
+    <div>
+      <p>I'm an about page!</p>
+      <p>I don't know what to put here!</p>
+    </div>
+  )
+}
+
+export default About
