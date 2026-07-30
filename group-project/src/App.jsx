2e2d194a8373f40d20a8d7ce9172ae0146b96674
Pft-86 <75560628+Pft-86@users.noreply.github.com>
2026-07-29 20:43:17 -0400

    Update
    Updated with BrowserRouter + routes

--- group-project/src/App.jsx
+++ group-project/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Menu from './pages/Menu'
 import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/"     element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
